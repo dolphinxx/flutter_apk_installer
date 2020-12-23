196af73ed01814dc20bab71b30cf51014a93541c
dolphin <adaikiss@163.com>
2020-12-23 12:25:15 +0800

    upgrade to v2 embedding

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A new Flutter plugin for android APK installation.
 
 ## Usage
 First download the APK to the app dir/external dir/cache dir of the device, then call
-`ApkInstaller.install(url)` to invoke installation.
+`ApkInstaller.install(apkPath)` to invoke installation.
 
 ## Note
 add `<uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE"/>` to `AndroidManifest.xml` if the APK is allocated in external dir.
--- android/src/main/java/com/whaleread/apkinstaller/ApkInstallerPlugin.java
+++ android/src/main/java/com/whaleread/apkinstaller/ApkInstallerPlugin.java
@@ -1,16 +1,17 @@
 package com.whaleread.apkinstaller;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
-import android.util.Log;
 
 import java.io.File;
-import java.util.Map;
 
+import androidx.annotation.NonNull;
 import androidx.core.content.FileProvider;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -20,23 +21,41 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 /**
  * ApkInstallerPlugin
  */
-public class ApkInstallerPlugin implements MethodCallHandler {
-    private Registrar registrar;
+public class ApkInstallerPlugin implements FlutterPlugin, MethodCallHandler {
+    private Context applicationContext;
+    private MethodChannel methodChannel;
 
     /**
      * Plugin registration.
      */
     public static void registerWith(Registrar registrar) {
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), "apk_installer");
-        channel.setMethodCallHandler(new ApkInstallerPlugin(registrar));
+        final ApkInstallerPlugin instance = new ApkInstallerPlugin();
+        instance.onAttachedToEngine(registrar.context(), registrar.messenger());
     }
 
-    public ApkInstallerPlugin(Registrar registrar) {
-        this.registrar = registrar;
+    public ApkInstallerPlugin() {
+    }
+
+    private void onAttachedToEngine(Context applicationContext, BinaryMessenger messenger) {
+        this.applicationContext = applicationContext;
+        this.methodChannel = new MethodChannel(messenger, "apk_installer");
+        this.methodChannel.setMethodCallHandler(this);
     }
 
     @Override
-    public void onMethodCall(MethodCall call, Result result) {
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        this.onAttachedToEngine(flutterPluginBinding.getApplicationContext(), flutterPluginBinding.getBinaryMessenger());
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        applicationContext = null;
+        methodChannel.setMethodCallHandler(null);
+        methodChannel = null;
+    }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
         if (call.method.equals("install")) {
             installPackage(call.<String>argument("path"), call.<String>argument("packageName"));
             result.success(null);
@@ -51,20 +70,17 @@ public class ApkInstallerPlugin implements MethodCallHandler {
             throw new RuntimeException(path + " not found!");
         }
         Intent intent = new Intent(Intent.ACTION_VIEW);
-        Context context = registrar.context();
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
             intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
         } else {
-            Uri fileUri = FileProvider.getUriForFile(context,
+            Uri fileUri = FileProvider.getUriForFile(applicationContext,
                     packageName,
                     file);
             intent.setDataAndType(fileUri, "application/vnd.android.package-archive");
             intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         }
-        if (!(context instanceof Activity)) {
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        }
-        context.startActivity(intent);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        applicationContext.startActivity(intent);
     }
 }
